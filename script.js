e4aa131ab23edb7a157e4e6f8188f07cc61995db
elisa lee <elisa.my.lee@gmail.com>
2016-09-04 14:27:13 -0500

    Initial Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var x = prompt("Please enter value 1.");
+var y = prompt("Please enter value 2. ");
+
+function splinter(value1, value2) {
+  var z = value1 * value2;
+  return result = [value1, value2, z];
+}
+
+alert(splinter(x, y));
